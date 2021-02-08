402007a3dcfb704b86a612eca28d9d648157c3ef
Ilya Belyavskiy <ilya.belyavskiy@swedbyte.se>
2021-02-08 20:03:45 +0300

    Avatar component sizes modificators

--- src/client/components/avatar/Avatar.tsx
+++ src/client/components/avatar/Avatar.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {Props} from './types';
 import './avatar.scss';
 
-const Avatar: Props = ({url, ...otherProps}) => {
+const Avatar: Props = ({url, size, ...otherProps}) => {
   const avatarStyle = url ? { backgroundImage: `url(${url})` } : {};
   return (
-    <div className="avatar" style={avatarStyle} {...otherProps}></div>
+    <div className={`avatar avatar_${size ? size : 'medium'}`} style={avatarStyle} {...otherProps}></div>
   );
 };
 
--- src/client/components/avatar/types.ts
+++ src/client/components/avatar/types.ts
@@ -2,6 +2,7 @@ import {FC, HTMLAttributes} from 'react';
 
 export type OwnProps = {
   url?: string;
+  size?: 'small' | 'medium' | 'large';
 } & HTMLAttributes<HTMLDivElement>;
 
 export type Props = FC<OwnProps>;
