3b0080f7d00f55e28d4ebc600955c6a79622fafb
ferahar <ferahar@gmail.com>
2021-02-08 17:28:52 +0500

    front-19-configuriring-router add components pages

--- src/client/components/app/App.tsx
+++ src/client/components/app/App.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import {
+  Home,
+  Game,
+  Profile,
+  SignIn,
+  SignUp,
+  Forum,
+  Leaderboard,
+} from '../../pages';
 
 export default function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/game">Game</Link></li>
+              <li><Link to="/signin">Вход</Link></li>
+              <li><Link to="/signup">Регистрация</Link></li>
+              <li><Link to="/profile">Профиль</Link></li>
+              <li><Link to="/forum">Форум</Link></li>
+              <li><Link to="/leaderboard">Рйтинг / top 10</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/game" component={Game} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/forum" component={Forum} />
+            <Route path="/leaderboard" component={Leaderboard} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
--- src/client/pages/index.ts
+++ src/client/pages/index.ts
@@ -1,1 +1,17 @@
-// here will go export for pages
+import Home from './home';
+import Game from './game';
+import SignIn from './signin';
+import SignUp from './signup';
+import Profile from './profile';
+import Leaderboard from './leaderboard';
+import Forum from './forum';
+
+export {
+  Home,
+  Game,
+  SignIn,
+  SignUp,
+  Profile,
+  Leaderboard,
+  Forum,
+};
