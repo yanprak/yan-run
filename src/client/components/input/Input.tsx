2c8d051ee44f56417127e73e78d7e74783533afb
ferahar <ferahar@gmail.com>
2021-02-09 18:26:16 +0500

    front-2. refactor to Class

--- src/client/components/input/Input.tsx
+++ src/client/components/input/Input.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Props } from './types';
 import './input.scss';
 
-const Input: React.FC<Props> = ({ title, errormessage, ...props }: Props) => (
-  <label className="input">
-    {title && <h6 className="input-title">{title}</h6>}
-    <input className="input-control" {...props} />
-    {errormessage && <span className="input-error">{errormessage}</span>}
-  </label>
-);
+class Input extends PureComponent<Props> {
+  render() {
+    const { title, errormessage, name, ...otherProps } = this.props;
+    return (
+      <label htmlFor={name} className="input">
+        {title && <h6 className="input-title">{title}</h6>}
+        <input className="input-control" id={name} {...otherProps} />
+        {errormessage && <span className="input-error">{errormessage}</span>}
+      </label>
+    );
+  }
+}
 
 export default Input;
