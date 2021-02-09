5324b29c7de3bbfe2d76540c183bc0d3dce63d22
ferahar <ferahar@gmail.com>
2021-02-09 23:26:43 +0500

    front-19-router. remove tag DIV

--- src/client/components/app/App.tsx
+++ src/client/components/app/App.tsx
@@ -18,31 +18,29 @@ import {
 
 export default function App() {
   return (
-    <div>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/game">Game</Link></li>
-              <li><Link to="/signin">Вход</Link></li>
-              <li><Link to="/signup">Регистрация</Link></li>
-              <li><Link to="/profile">Профиль</Link></li>
-              <li><Link to="/forum">Форум</Link></li>
-              <li><Link to="/leaderboard">Рйтинг / top 10</Link></li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/game" component={Game} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/forum" component={Forum} />
-            <Route path="/leaderboard" component={Leaderboard} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/game">Game</Link></li>
+            <li><Link to="/signin">Вход</Link></li>
+            <li><Link to="/signup">Регистрация</Link></li>
+            <li><Link to="/profile">Профиль</Link></li>
+            <li><Link to="/forum">Форум</Link></li>
+            <li><Link to="/leaderboard">Рйтинг / top 10</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/game" component={Game} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/forum" component={Forum} />
+          <Route path="/leaderboard" component={Leaderboard} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
