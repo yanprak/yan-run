209e3fbb70e2cd7a8d13dde02249e4662e23ebe0
Ilya <iqwerty363@gmail.com>
2021-02-15 23:53:20 +0300

    Front 23 components topic title (#14)
    * front-23-forum Topic Title component

--- src/client/pages/forum/Forum.tsx
+++ src/client/pages/forum/Forum.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import TopicLink from '../../components/topic-link';
 
 export default function Forum() {
   return (
-    <div>
-      <h1>Форум</h1>
+    <div className="page container container_is-column container_size-auto container_center">
+      <TopicLink title="Новые игры" messagesCounter={333} />
+      <TopicLink title="Геймдизайнеры" messagesCounter={222} />
+      <TopicLink title="Технологии" messagesCounter={111} />
+      <TopicLink title="Cyperpunk 2077" messagesCounter={2048} />
+      <TopicLink title="Tetris" messagesCounter={5} />
     </div>
   );
 }
