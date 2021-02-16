320a8319c5cbb7bcff819fb5568f5ab7eb47da0d
Ilya Belyavskiy <ilya.belyavskiy@swedbyte.se>
2021-02-16 22:05:00 +0300

    Front 14 Page Topic - added pagination and 'Delete topic' in case the user is the topic starter. Had no idea where to get this data from though, so temporarily placed it in to the page component props and created the types.ts file

--- src/client/pages/topic/Topic.tsx
+++ src/client/pages/topic/Topic.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ForumMessage from '../../components/forum-message';
+import Pagination from '../../components/pagination';
+import Button from '../../components/button';
+import { TopicPageProps } from './types';
+import './topic.scss';
 import sampleMessages from './sampleMessages';
 
-export default function Topic() {
+export default function Topic(props: TopicPageProps) {
   return (
-    <div className="forum-topic page container container_is-column container_center">
-      { sampleMessages.map(messageObj => <ForumMessage {...messageObj} />) }
+    <div className="page topic-page container container_is-column container_center">
+      <div className="margin_tb_s-7 topic-page__header">
+        <Pagination path="/topic" current={1} total={5} className="topic-page__pagination" />
+        { props.isAuthor && <Button size="small" styleType="secondary" className="topic-page__button">Удалить тему</Button> }
+      </div>
+      <hr />
+      <div className="topic-page__messages">
+        { sampleMessages.map(messageObj => <ForumMessage {...messageObj} />) }
+      </div>
     </div>
   );
 }
