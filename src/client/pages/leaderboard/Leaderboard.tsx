3574079d3e884cddbcf794586e11da57f4e43c88
antonkagakin <74297088+antonkagakin@users.noreply.github.com>
2021-02-16 23:08:06 +0800

    Front-15 leaderboard implementation (#18)
    Front-15 leaderboard implementation

--- src/client/pages/leaderboard/Leaderboard.tsx
+++ src/client/pages/leaderboard/Leaderboard.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, {
+  memo, useCallback, useEffect, useState,
+} from 'react';
 
-export default function Leaderboard() {
+import './leaderboard.scss';
+import LeaderboardRow from './LeaderboardRow';
+import { LeaderboardRowProps } from './types';
+
+/*
+    todo(anton.kagakin)
+    мусор в виде getRandomInt, getRandomName, fakeData, setTimeout умрет при подключении API для leaderboard
+ */
+
+function getRandomInt(min = 1, max: number): number {
+  return Math.floor(Math.random() * max + min);
+}
+
+function getRandomName(): string {
+  const newArray = new Array(getRandomInt(1, 25)) as [];
+  return [...newArray].map(() => Math.random().toString(36)[2]).join('');
+}
+
+const fakeData: LeaderboardRowProps[] = new Array(5).fill(0).map((_, index) => ({
+  rank: index + 1,
+  user: {
+    avatar: 'https://yastatic.net/q/praktikum/v0.169.7/static/favicon.png',
+    login: getRandomName(),
+  },
+  score: Math.floor(Math.random() * 99999),
+}));
+
+function Leaderboard() {
+  const [data, setData] = useState<LeaderboardRowProps[]>([]);
+
+  // componentDidMount
+  useEffect(() => {
+    setTimeout(setData, 700, fakeData);
+  }, []);
+
+  const createRows = useCallback(() => data.map(item => (
+    <LeaderboardRow key={item.rank} {...item} />
+  )), [data]);
+
   return (
-    <div>
-      <h1>Рейтинг игроков</h1>
+    <div className="container container_center container_center-items">
+      <div className="leaderboard">
+        {data.length ? createRows() : 'Loading'}
+      </div>
     </div>
   );
 }
+
+export default memo(Leaderboard);
---
+++ src/client/pages/leaderboard/types.ts
@@ -0,0 +1,8 @@
+export type LeaderboardRowProps = {
+  rank: number,
+  user: {
+    avatar: string,
+    login: string
+  }
+  score: number
+};
