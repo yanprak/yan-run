fe046bfbeca7b3857c87f72f429149ed945c761e
ferahar <69187061+ferahar@users.noreply.github.com>
2021-02-18 19:39:59 +0500

    Env 2 heroku (#20)
     env-2-heroku:
    – edit server.ts
    – edit Procfile
    –  add command "build" to Procfile
    Co-authored-by: ferahar <ferahar@gmail.com>

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,15 @@
-import express from 'express';
-
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 4100;
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+type Res = ReturnType<typeof app.response>
+type Req = ReturnType<typeof app.request>
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (request: Req, response: Res) => {
+  response.sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.listen(PORT, () => {
