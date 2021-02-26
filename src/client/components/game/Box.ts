072b1031617a7b292564ba31cc1d6893103aa35e
ferahar <69187061+ferahar@users.noreply.github.com>
2021-02-26 18:47:53 +0500

    Env 4 webpack file loader and texturing box (#31)
    * env-4-webpack_file-loader_texturing
    – add file-loader for webpack
    – add types: png image modul
    – add Box texture
    – add Player texture and animate
    – edit Box
    – edit Player texture and animate
    Co-authored-by: ferahar <ferahar@gmail.com>

--- src/client/components/game/Box.ts
+++ src/client/components/game/Box.ts
@@ -1,8 +1,11 @@
-import { PropsGameObject, Config, CTX } from './type';
+import { Config, CTX, PropsGameObject } from './type';
 import Player from './Player';
+
+import boxImage from './textures/box.png';
 
 export default class Box {
   ctx: CTX;
+  img: HTMLImageElement = new Image();
 
   public state: PropsGameObject = {
     x: 800,
@@ -17,26 +20,25 @@ export default class Box {
     this.config = config;
     this.ctx = ctx;
     this.state.x = config.box_x;
+    this.img = new Image();
+    this.img.src = boxImage;
   }
 
   public show() {
     if (!this.ctx) return;
-    this.ctx.fillStyle = 'gray';
     const { x, y, w, h } = this.state;
-    this.ctx.fillRect(x, y, w, h);
+    this.ctx.drawImage(this.img, 0, 0, w, h, x, y, w, h);
   }
 
   public update() {
     const p = (this.config.player as Player).state;
     const { x, y, w, h } = this.state;
 
-    // Эту проверку потом вытащу
-    const isUpdate = (
+    return (
       x < (p.x + p.w)
       && (x + w) > p.x
       && y < (p.y + p.h)
       && (y + h) > p.y
     );
-    return isUpdate;
   }
 }
--- src/client/components/game/Player.ts
+++ src/client/components/game/Player.ts
@@ -1,37 +1,56 @@
 import { PropsGameObject, Config, CTX } from './type';
 
+import runImageSprite from './textures/sprite/frog/run.png';
+
 export default class Player {
   ctx: CTX;
+  img: HTMLImageElement = new Image();
 
   public state: PropsGameObject = {
     x: 100,
     y: 100,
-    w: 40,
-    h: 80,
+    w: 48,
+    h: 48,
   };
 
   ySpeed = 5;
 
-  config: Config;
+  private config: Config;
+  private maxWidthImage = 576;
+  private tickCount = 0;
+  private ticksPerFrame = 2;
+  private frameIndex = 0;
+  private frame = 12;
 
   constructor(config: Config, ctx: CTX) {
     this.config = config;
     this.ctx = ctx;
+    this.img = new Image();
+    this.img.src = runImageSprite;
   }
 
   public show() {
     if (!this.ctx) return;
-    // цвет и размеры будут переработаны после анимации персонажа и заключены в конфиг
-    this.ctx.fillStyle = '#5b8a20';
     const { x, y, w, h } = this.state;
-    this.ctx.fillRect(x, y, w, h);
+    this.tickCount++;
+    if (this.tickCount > this.ticksPerFrame) {
+      this.tickCount = 0;
+      if (this.frameIndex < this.frame - 1) {
+        this.frameIndex++;
+      } else {
+        this.frameIndex = 0;
+      }
+    }
+    const sX = (this.frameIndex * this.maxWidthImage) / this.frame;
+    this.ctx.drawImage(this.img, sX, 0, w, h, x, y, w, h);
   }
 
   public update() {
-    this.state.y += this.ySpeed;
+    this.state.y = Math.round(this.state.y + this.ySpeed);
     this.ySpeed += this.config.gravity;
     const bg = this.config.background;
-    if (this.state.y >= bg.y - this.state.h) {
+    const lowerBorder = Math.round(bg.y - this.state.h);
+    if (this.state.y > lowerBorder) {
       this.ySpeed = 0;
       this.config.canJump = true;
     } else {
