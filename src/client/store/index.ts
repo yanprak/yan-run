faec2823ff6e9c5c061cdf2d3f26bcec15df23d5
antonkagakin <kagakinam@yandex.ru>
2021-05-01 03:00:54 +0800

    midnight test fix

--- src/client/store/index.ts
+++ src/client/store/index.ts
@@ -10,7 +10,7 @@ import middlewares from './middlewares';
   Initial state can be 'undefined' in case the user visits for the first time (there is no localStorage record yet).
   Second argument (initialState) of 'createStore' can be undefined, [], {} & fn.
 */
-export default function configureStore(initialState: ApplicationState | undefined, url = '/') {
+export default function configureStore(initialState: ApplicationState | undefined, url = '') {
   const history = isServer
     ? createMemoryHistory({ initialEntries: [url] })
     : createBrowserHistory();
